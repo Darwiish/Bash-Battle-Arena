a30977cb918acdfcde954c928001dc6f91637331
Darwiish <inaaboowe@gmail.com>
2026-02-19 03:03:18 +0400

    Bash scripts later on

---
+++ level1_.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Level 1: The Basics "
+echo "Mission: Create a directory named Arena and then inside it, create three files: warrior.txt, mage.txt, and archer.txt."
+echo "Then list the contents of the Arena directory."
+
+# Create the directory
+mkdir Arena
+
+# Move into the directory
+cd Arena
+
+# Create the three files
+touch warrior.txt mage.txt archer.txt
+
+# List the contents to check
+ls # List the contents to check
+
+#one-go
+mkdir -p Arena && cd Arena && touch warrior.txt mage.txt archer.txt && ls
---
+++ level2_.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#echo " Level 2: Variables and Loops "
+#echo "Mission: Create a script that outputs the numbers 1 to 10, one number per line."
+echo
+
+for i in {1..10}; do
+	echo "Count: $i"
+done
+
+echo
+
+count=1
+while [ $count -le 10 ]; do
+	echo "Count: $count"
+	((count++))
+done
