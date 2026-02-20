5a7995827382a6ef361186a65d8c666c2644fb94
Darwiish <inaaboowe@gmail.com>
2026-02-21 02:28:11 +0400

    Bash script demonstrating variables and loops (for & while loops)

--- level2_.sh
+++ level2_.sh
@@ -4,12 +4,14 @@
 # Mission: Create a script that outputs the numbers 1 to 10, one number per line.
 echo
 
+# Loop from 1 to 10 using brace expansion
 for i in {1..10}; do
 	echo "Count: $i"
 done
 
 echo
 
+# Loop from 1 to 10 using a counter variable
 count=1
 while [ $count -le 10 ]; do
 	echo "Count: $count"
