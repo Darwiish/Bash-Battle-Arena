da663c212ed11154281dc9bdebfa0b34b5d5e69d
Darwiish <inaaboowe@gmail.com>
2026-03-07 02:52:53 +0400

    Add bash script to check for hero.txt using conditional statements

--- level3_.sh
+++ level3_.sh
@@ -3,6 +3,7 @@
 # Level 3: Conditional Statements
 # Mission: Write a script that checks if a file named hero.txt exists in the Arena directory.
 # If it does, print Hero found!; otherwise, print Hero missing!.
+
 echo
 
 TARGET_FILE="Arena/hero.txt"
